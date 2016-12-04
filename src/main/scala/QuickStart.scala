b2706d62c2fa58b182555f6e2ffd29f59e8c171e
Mike Slinn <mslinn@micronauticsresearch.com>
2016-12-04 14:42:09 -0800

    -

--- src/main/scala/QuickStart.scala
+++ src/main/scala/QuickStart.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl._
 import akka.util.ByteString
-import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object QuickStart extends App {
   implicit val system = ActorSystem("QuickStart")
@@ -12,11 +12,11 @@ object QuickStart extends App {
 
   val source: Source[Int, NotUsed] = Source(1 to 100)
   source.runForeach(i => println(i))(materializer)
-
-  val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
-
-  val result: Future[IOResult] =
-    factorials
-      .map(num => ByteString(s"$num\n"))
-      .runWith(FileIO.toPath(Paths.get("factorials.txt")))
+  val factorials: Source[BigInt, NotUsed] = source.scan(BigInt(1))((acc, next) => acc * next)
+  factorials
+    .map(num => ByteString(s"$num\n"))
+    .runWith(FileIO.toPath(Paths.get("factorials.txt")))
+    .andThen { // just for interest's sake
+      case scala.util.Success(x) => println(x)
+    }
 }
